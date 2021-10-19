bd1286433ca8537239323fcc964c9affb05a81fc
VascoMS <73793922+VascoMS@users.noreply.github.com>
2021-10-19 21:25:38 +0100

    Added class date incomplete

---
+++ ggc-skeleton/ggc/core/Date.java
@@ -0,0 +1,24 @@
+package ggc.core;
+import java.lang.Math;
+
+public class Date {
+    private int _days;
+
+    public Date(int days){
+        _days = days;
+    }
+    public int getDays(){
+        return _days;
+    }
+
+    public Date add(int days){
+        _days += days;
+        return this;
+    }
+
+    public int difference(Date other){
+        return Math.abs(other.getDays()-_days);
+    }
+
+    public static Date now(){return Date(_days);}
+}
